d7fdd5202860319aabcd005b90020f22c2535e49
Wanchao Zhang <wonderbeyond@gmail.com>
2015-03-21 16:08:41 +0800

    enhancement

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
 $(function() {
+    // photo slides
+    var curPtr = -1;
+    var $photo = $('#overview .photo');
+    (function swapPhoto() {
+        var photos = [
+            'photos/wonder-md.jpg',
+            'photos/wonder-rainbow.jpg',
+            'photos/wonder-hsjq.jpg',
+        ];
+        if(curPtr >= photos.length - 1) {
+            console.log('new turn');
+            curPtr = 0;
+        } else {
+            curPtr += 1;
+        }
+
+        if($photo.attr('src') !== photos[curPtr]) {
+            $photo.fadeOut('fast', function(){
+                $photo.attr('src', photos[curPtr]);
+                $photo.fadeIn('slow');
+            });
+        }
+        setTimeout(swapPhoto, 3500);
+    })();
+
+
+    // Skill list
     $('.skill-list li').each(function(i,e) {
         var grade = e.dataset.grade;
         $(this).addClass('text-energized').css({
